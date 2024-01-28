71eebef080198f28636cea768542187361a7a67a
yehuda <yehudarothstein@gmail.com>
2024-01-28 19:43:58 +0200

    Added Admin /Redirect

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 #CoBranch
 app = Flask(__name__)
 
 
 def home():
     return "Welcome to Excalibur 6738 ScoutSystem <H1>6738<H1>"
+@app.route("/admin")
+def admin(name):
+     return redirect(url_for("/"))
 
 
 if __name__ == __name__:
