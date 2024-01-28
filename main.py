c792df59f69fde2e899c411b87ea8cd12466c9b5
Ori Korngut <ori.korngut@gmail.com>
2024-01-28 21:17:08 +0200

    Basic

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-from flask import Flask, redirect, url_for
-#CoBranch
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+@app.route("/<name>")
+def home(name):  # Change the parameter name to match the route
+    return render_template("index.html", content=name)  # Adjust the parameter name here as well
 
-@app.route("/")
-def home():
-    return "Welcome to Excalibur 6738 ScoutSystem <H1>6738<H1>"
-@app.route("/admin")
-def admin(name):
-     return redirect(url_for("/"))
-
-
-if __name__ == __name__:
+if __name__ == '__main__':
     app.run()
