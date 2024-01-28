30123231f3915552312f15c4a643c4aed5b44794
Ori Korngut <ori.korngut@gmail.com>
2024-01-28 22:11:36 +0200

    Add report Bugs

--- main.py
+++ main.py
@@ -12,6 +12,18 @@ def home_with_name(name):
 def test():
     return render_template("new.html")
 
+@app.route("/test")
+def test():
+    return render_template("new.html")
+
+@app.route("/ReportBugs", methods=["POST", "GET"])
+def ReportBug():
+    return render_template("Report.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
 
 if __name__ == '__main__':
     app.run()
