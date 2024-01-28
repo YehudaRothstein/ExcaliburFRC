ecd6e13fd2c59c81a1493dc5fc409b5e3acbd0e6
Ori Korngut <ori.korngut@gmail.com>
2024-01-28 21:42:51 +0200

    Working NeigateBar

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/<name>")
-def home(name):  # Change the parameter name to match the route
-    return render_template("index.html", content=name)  # Adjust the parameter name here as well
+def home_with_name(name):
+    return render_template("index.html")
+
+
+@app.route("/test")
+def test():
+    return render_template("new.html")
+
 
 if __name__ == '__main__':
     app.run()
