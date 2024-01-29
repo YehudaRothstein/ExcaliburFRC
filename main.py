9d53b6ad35abae74a3d338c2328b35df80f287f1
excalibur <ExcaliburFRC@gmail.com>
2024-01-29 12:33:28 +0200

    Go home

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import os
+
 from flask import Flask, render_template, redirect, url_for, request, jsonify
 import json
+
+from pip._internal.utils import datetime
 
 app = Flask(__name__)
 
@@ -7,7 +11,7 @@ JSON_FILE_PATH = "form_data.json"
 
 
 def update_json_file(data):
-    with open(JSON_FILE_PATH, 'w') as json_file:
+    with open("C:\\Users\\excal\\PycharmProjects\\ExcaliburFRC\\Scout\\Data.JSON", 'w') as json_file:
         json.dump(data, json_file)
 
 
@@ -53,24 +57,23 @@ def process_form():
     qual_number = request.form.get("qual_number")
     team = request.form.get("team")
 
-    Data = {
+    data = {
         "amp_value": amp_value,
         "speaker_value": speaker_value,
         "qual_number": qual_number,
         "team": team
     }
 
-    update_json_file(Data)
+    # Generate a filename based on the current date and time
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    json_filename = f"Data_{timestamp}.json"
+    json_filepath = os.path.join("C:\\Users\\excal\\PycharmProjects\\ExcaliburFRC\\Scout\\Data.JSON")
+
+    # Save data to the JSON file
+    update_json_file(data, json_filepath)
 
     return redirect(url_for('Autonomus'))
 
 
-@app.route("/get_form_data", methods=["GET"])
-def get_form_data():
-    with open(JSON_FILE_PATH, 'r') as json_file:
-        data = json.load(json_file)
-    return jsonify(data)
-
-
 if __name__ == '__main__':
-    app.run(host="10.97.227.171", port=5000)
+    app.run()
