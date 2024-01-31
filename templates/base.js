85af278d8ce8a151491a26b2358232c9ea773c8c
Team <yehudarothstein@gmail.com>
2024-01-31 17:16:17 +0200

    Cleaned Up Code

--- templates/base.js
+++ templates/base.js
@@ -1,23 +1,10 @@
-  function updateCounter(change, counterId, category) {
-        let counterValue = parseInt(document.getElementById(counterId).value);
-        counterValue = Math.max(0, counterValue + change);
-        document.getElementById(counterId).value = counterValue;
+function changeValue(elementId, amount) {
+    const currentValue = parseInt(document.getElementById(elementId).innerText);
+    const newValue = currentValue + amount;
 
-        let totalCounterId = 'totalCounter' + category;
-        let totalCounterValue = parseInt(document.getElementById(totalCounterId).value);
-        totalCounterValue = Math.max(0, totalCounterValue + change);
-        document.getElementById(totalCounterId).value = totalCounterValue;
-    }
-
-           function changeValue(elementId, amount) {
-            var currentValue = parseInt(document.getElementById(elementId).innerText);
-            var newValue = currentValue + amount;
-
-            document.getElementById(elementId).innerText = newValue;
+    document.getElementById(elementId).innerText = newValue;
 
             document.m("hidden_" + elementId).value = newValue;
+
         }
 
-        function updateCounter(change, counterId, category) {
-
-}
